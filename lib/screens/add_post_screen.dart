3efeec31aad5dbbe09d00cd31911cb71d4a34353
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-08-25 23:56:24 +0300

    AddPostScreen

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:instagramclone/models/users.dart';
+import 'package:instagramclone/providers/user_provider.dart';
 import 'package:instagramclone/utils/color.dart';
 import 'package:instagramclone/utils/utils.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -40,7 +43,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
               onPressed: () async {
                 Navigator.of(context).pop();
                 Uint8List file = await pickImage(
-                  ImageSource.camera,
+                  ImageSource.gallery,
                 );
                 setState(() {
                   _file = file;
@@ -55,11 +58,13 @@ class _AddPostScreenState extends State<AddPostScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final User user = Provider.of<UserProvider>(context).getUser!;
+
     return _file == null
         ? Center(
             child: IconButton(
               icon: const Icon(Icons.abc),
-              onPressed: () {},
+              onPressed: () => _selectImage(context),
             ),
           )
         : Scaffold(
