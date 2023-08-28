98a56dcee375cca3895c4d48c6a49f8267bb2931
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-08-29 00:49:09 +0300

    postCard

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -65,10 +65,111 @@ class PostCard extends StatelessWidget {
                             ));
                   },
                   icon: const Icon(Icons.more_vert_outlined),
-                )
+                ),
               ],
             ),
           ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              "https://plus.unsplash.com/premium_photo-1690921287957-016981122c78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+              fit: BoxFit.cover,
+            ),
+          ),
+          // Like Commnet
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite_outlined,
+                  color: Colors.red,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.comment_outlined,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.send,
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.bookmark_border_outlined,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                  style: (Theme.of(context).textTheme.labelMedium ??
+                          const TextStyle())
+                      .copyWith(fontWeight: FontWeight.bold),
+                  child: Text(
+                    "1,231 likes",
+                    style: Theme.of(context).textTheme.bodySmall ??
+                        const TextStyle(),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(top: 8),
+                  child: RichText(
+                    text: const TextSpan(
+                        style: TextStyle(color: primaryColor),
+                        children: [
+                          TextSpan(
+                            text: "username ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: "comment",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )
+                        ]),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: const Text(
+                      "view all 200 comments",
+                      style: TextStyle(
+                          color: secondaryColor, fontWeight: FontWeight.w200),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: const Text(
+                    "29/09/2023",
+                    style: TextStyle(
+                        color: secondaryColor, fontWeight: FontWeight.w200),
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
