764190d6893fb4358dd2f90c6daed797d4400559
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-09-04 23:10:19 +0300

    commentsScreen

---
+++ lib/screens/comments_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommentsScreen extends StatefulWidget {
+  const CommentsScreen({super.key});
+
+  @override
+  State<CommentsScreen> createState() => _CommentsScreenState();
+}
+
+class _CommentsScreenState extends State<CommentsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
