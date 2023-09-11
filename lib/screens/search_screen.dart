74aae9301c5dcf51ef553df21c5ebd60b71b5f73
Zeki Furkan Durum <138057214+zfurkandurum@users.noreply.github.com>
2023-09-11 23:05:14 +0300

    searchScreen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instagramclone/utils/color.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,8 +10,58 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchEditingController = TextEditingController();
+  bool isShowUser = false;
+  @override
+  void dispose() {
+    super.dispose();
+    searchEditingController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: TextFormField(
+            decoration: const InputDecoration(
+              labelText: "search for a user",
+            ),
+            onFieldSubmitted: (String _) {
+              setState(() {
+                isShowUser = true;
+              });
+            },
+          ),
+        ),
+        body: isShowUser
+            ? FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .where('username',
+                        isGreaterThanOrEqualTo: searchEditingController.text)
+                    .get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView.builder(
+                      itemCount: (snapshot.data! as dynamic).docs.lenght,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                (snapshot.data! as dynamic).docs[index]
+                                    ['photoUrl']),
+                          ),
+                          title: Text((snapshot.data! as dynamic).docs[index]
+                              ['username']),
+                        );
+                      });
+                },
+              )
+            : const Text(""));
   }
 }
